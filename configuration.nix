82fb2a363b932b58281da8ca755c77f3611d9b0a
kabimaru-dev <viacheslavchuebnko@animesakura.net>
2026-07-24 13:38:28 +0200

    v0.0.1 - initialization

--- configuration.nix
+++ configuration.nix
@@ -1,110 +1,147 @@
-{ inputs, lib, config, pkgs, ... }: 
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
-  imports = [
-    ./hardware-configuration.nix
-  ];
-
+  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+
+  # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
   
-  time.timeZone = "Europe/Kyiv";
 
-  # Enter keyboard layout
-  services.xserver.layout = "us";
-  services.xserver.xkbVariant = "altgr-intl";
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
-  # Define user accounts
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.Gabimaru = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
-      fastfetch
+      # Window Managers
+      sway
+
+      # Software Environment
+      git gh vscodium
+
+      # Browsers
+      firefox tor-browser 
+
+      # Login
+      greetd tuigreet
+
+      # Drivers
+      mesa
+
+      # Terminals
+      foot
+
+      # Volume
+      pavucontrol
+      
+      # Brightness
+      brightnessctl
     ];
   };
-<<<<<<< HEAD
 
-  users.extraGroups.docker.members = [ "username-with-access-to-socket" ];
-  virtualisation.docker.storageDriver = "btrfs";
-  services.gnome.gnome-keyring.enable = true;
+  # programs.firefox.enable = true;
 
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-  };
-
-  services.greetd = {                                                      
-    enable = true;                                                         
-    settings = {                                                           
-      default_session = {                                                  
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Gnome";
-        user = "greeter";                                                  
-      };                                                                   
-    };                                                                     
-  };
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6b8e892 (second)
-  services.desktopManager.gnome.enable = true; # Enable Gnome
-=======
-
-  services.desktopManager.gnome.enable = true;
-
->>>>>>> 10717e1 (fifth)
-  services.postfix = {
-    enable = true;
-    hostname = "nixos.animesakura.net";
-    
-    settings.main = {
-      relayhost = ["[smtp.sendgrid.net]:587"];
-    };
-    
-    config = {
-      smtp_sasl_auth_enable = "yes"; # SASL auth
-      # change me this <api-key> here: smtp_sasl_password_maps = "inline:{smtp.sendgrid.net=apikey:<api-key>}";
-      smtp_sasl_security_options = "noanonymous";
-      smtp_use_tls = "yes";
-      smtp_tls_security_level = "encrypt";
-      smtp_tls_note_starttls_offer = "yes";
-    };
-  };
-<<<<<<< HEAD
-<<<<<<< HEAD
-  # programs.mtr.enable = true; # Some programs need SUID wrappers, can be configured further or are
-  # programs.gnupg.agent = {    # started in user sessions.
-<<<<<<< HEAD
-=======
-
-  # Enable Gnome
-  services.desktopManager.gnome.enable = true;
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  # environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   wget
+  # ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
->>>>>>> 9dc6211 (second (very old, alpha))
-=======
->>>>>>> 6b8e892 (second)
   #   enable = true;
   #   enableSSHSupport = true;
-  # };  
-  # services.openssh.enable = true; # List services that you want to enable:  # Enable the OpenSSH daemon.
-  # networking.firewall.allowedTCPPorts = [ ... ];                            # Open ports in the firewall.
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # networking.firewall.enable = false;                                       # Or disable the firewall altogether.
-  # system.copySystemConfiguration = true; # Copy the NixOS configuration file and link it from the resulting system # (/run/current-system/configuration.nix). This is useful in case you # accidentally delete configuration.nix.
-  system.stateVersion = "26.05"; # Do NOT change this value # see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion . # Did you read the comment?
-}
-=======
->>>>>>> 10717e1 (fifth)
+  # };
 
-=======
-  
->>>>>>> 99bb00b (sixth)
-  system.stateVersion = "26.05";
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.11"; # Did you read the comment?
+
 }
+
