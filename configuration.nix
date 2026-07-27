4942ad7f6fe353a28ca66f757f24534169213678
kabimaru-dev <viacheslavchubenko@animesakura.net>
2026-07-27 09:09:21 +0000

    structured code and normalize packages

--- configuration.nix
+++ configuration.nix
@@ -1,56 +1,57 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_hardened;
+  };
 
   networking.networkmanager.enable = true;
 
-  services.pipewire.enable = true;
-  services.pipewire.pulse.enable = true;
+  services = {
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+    };
 
-  programs.sway.enable = true;
-  programs.sway.wrapperFeatures.gtk = true;
+    greetd = {
+      enable = true;
+      settings.default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd GNOME";
+        user = "greeter";
+      };
+    };
 
-  services.greetd = {
-    enable = true;
-    settings.default_session = {                                                  
-      command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd GNOME";
-      user = "greeter";                                                  
+    desktopManager.gnome.enable = true;
+    gnome.gnome-keyring.enable = true;
+  };
+
+  users.users = {
+    Gabimaru = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      packages = with pkgs; [
+        git gh zed-editor               # Develop
+        fastfetch gparted pavucontrol   # Linux
+        mesa                            # Drivers
+        firefox tor-browser             # Browsers
+      ];
     };
   };
 
-  services.desktopManager.gnome.enable = true;
-  services.gnome.gnome-keyring.enable = true;
 
-  users.users.Gabimaru = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    packages = with pkgs; [
-      # Software Environment
-      git gh vscodium
-
-      # Linux Environment
-      fastfetch
-
-      # Browsers
-      firefox tor-browser 
-
-      # Drivers
-      mesa
-
-      # Volume
-      pavucontrol
-    ];
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
   };
 
   system.stateVersion = "25.11";
 }
-
