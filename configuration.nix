cbf9674602f72e806c369d5a9da9858391608241
kabimaru-dev <viacheslavchubenko@animesakura.net>
2026-07-29 19:21:00 +0000

    test2

--- configuration.nix
+++ configuration.nix
@@ -12,27 +12,6 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-	
-	#kernelPackages = pkgs.linuxPackages_latest;
-	#kernelPackages = pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_19.override {
-	#	argsOverride = rec {
-	#		src = pkgs.fetchurl {
-	#		    url = "mirror://kernel/linux/kernel/v6.x/linux-6.19.tar.xz";
-	#		    sha256 = "303079a8250b8f381f82b03f90463d12ac98d4f6b149b761ea75af1323521357";
-	#		};
-	#		version = "6.19";
-	#		modDirVersion = "6.19";
-	#	};
-	#});
-    
-    #kernelPatches = [
-	#{
-	#	name = "CVE-2026-31431-copy-fail";
-	#	patch = ./kernel-security-patches/CVE-2026-31431-copy-fail.patch;
-	#	structuredExtraConfig.CVE-2026-31431-copy-fail = lib.kernel.yes;
-	#	features.CVE-2026-31431-copy-fail = true;
-	#}
-    #];
   };
 
   networking.networkmanager.enable = true;
@@ -53,7 +32,7 @@
     
     xserver = {
       enable = true;
-      layout = "us, ru";
+      layout = "us,ru";
       xkbOptions = "grp:alt_shift_toggle";
       libinput = {
       	enable = true;
@@ -69,13 +48,13 @@
       isNormalUser = true;
       extraGroups = [ "wheel" ];
       packages = with pkgs; [
-        git gh python3 python313Packages.pip pipx zed-editor android-tools 	# Develop
-        androidStudioPackages.stable clang					# 
+        git gh python3 python313Packages.pip pipx lapce android-tools                   # Develop
+        androidStudioPackages.stable clang                                              # 
         
-        fastfetch gparted pavucontrol   					# Linux
-        mesa                            					# Drivers
-        firefox tor-browser             					# Browsers
-        telegram-desktop							# etc
+        fastfetch gparted pavucontrol tree                                              # Linux
+        mesa                                                                            # Drivers
+        firefox tor-browser                                                             # Browsers
+        telegram-desktop obs-studio                                                     # etc
       ];
     };
   };
