7be2825cb9cd3b73dc9dbabf9e3bdaea39ba01af
kabimaru-dev <viacheslavchubenko@animesakura.net>
2026-08-31 11:17:58 +0000

    Fix: Keyboard

--- configuration.nix
+++ configuration.nix
@@ -1,16 +1,16 @@
 { config, lib, pkgs, ... }:
-{   
-  # virtualisation.docker.enable = true;
-  # security.pam.services.Gabimaru.enableGnomeKeyring = false;
-  security.polkit.enable = true;
-  networking.networkmanager.enable = true;
+{ 
+  # Choose WM
+  # services.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
 
+  # Imports
   imports = [
     ./hardware-configuration.nix
   ];
 
 
-
+  # Boot
   boot = {
     loader = {
       systemd-boot.enable = true;
@@ -19,6 +19,7 @@
   };
 
 
+  # Services
   services = {
     pipewire = {
       enable = true;
@@ -38,26 +39,42 @@
       enable = true;
       layout = "us,ru";
       xkbOptions = "grp:alt_shift_toggle";
-      libinput.enable = true;
     };
 
-    desktopManager.gnome.enable = true;
+    power-profiles-daemon = {
+      enable = true;
+    };
+
+    upower = {
+      enable = true;
+    };
+
+    libinput = {
+      enable = true;
+    };
+
     # gnome.gnome-keyring.enable = true;
   };
 
 
+  # Insecure Packages
   nixpkgs.config = { allowUnfree = true; permittedInsecurePackages = [ 
       "electron-39.8.10"
     ];
   };
+  # All Packages
   users.users = {
     user = {
       isNormalUser = true;
-      extraGroups = [ "wheel" ];
+      extraGroups = [ "wheel" "input" ];
       packages = with pkgs; [
         # Niri
-        niri alacritty swaylock grim slurp foot cliphist fuzzel mako swaybg 
-        xwayland-satellite xdg-desktop-portal-gnome
+        niri alacritty kitty swaylock grim slurp foot cliphist fuzzel mako swaybg 
+        xwayland-satellite xdg-desktop-portal-gnome noctalia-shell
+
+
+        # Keyboard
+        ibus 
 
 
         git gh /* python3 python313Packages.pip pipx */ vscodium nil android-tools            # Develop
@@ -66,12 +83,12 @@
         wayland-scanner libxcb libX11 libXau libXdmcp libXrandr wayland 
         wayland-protocols mesa-demos virtualgl virtualglLib vulkan-validation-layers
         libGL (lib.getDev glfw) libglvnd (lib.getDev libglvnd) glfw3 libgcc gcc
-        gdb gdbHostCpuOnly shaderc glslang
+        gdb gdbHostCpuOnly shaderc glslang 
 
 
         fastfetch gparted pavucontrol tree peazip zip unzip gnome-extension-manager           # Linux
         gnome-tweaks busybox-sandbox-shell pciutils yad coreutils gnutar gnused bash
-        wget curl xdg-utils gtk3 gdk-pixbuf glib wine
+        wget curl xdg-utils gtk3 gdk-pixbuf glib wine libinput wev libxkbcommon
         
         
         mesa                                                                                  # Drivers
@@ -88,6 +105,7 @@
   };
 
 
+  # Programs
   programs = {
     sway = {
       enable = true;
@@ -96,10 +114,13 @@
     niri = {
       enable = true;
     };
-    nix-ld.enable = true;
+    nix-ld = {
+      enable = true;
+    };
   };
 
 
+  # Nix
   nix = {
     # i should to change it
     gc = {
@@ -120,6 +141,7 @@
   };
 
 
+  # Hardware
   hardware = {
     nvidia = {
       package = config.boot.kernelPackages.nvidiaPackages.stable;
@@ -141,13 +163,47 @@
   };
 
 
-  environment.variables = {
-    __NV_PRIME_RENDER_OFFLOAD = "1";
-    __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  # Env
+  environment = {
+    variables = {
+      __NV_PRIME_RENDER_OFFLOAD = "1";
+      __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    };
+    sessionVariables = {  
+      XKB_DEFAULT_LAYOUT = "us,ru";  
+      XKB_DEFAULT_OPTIONS = "grp:alt_shift_toggle";
+    };
   };
 
 
 
+
+
+
+
+  # Allow Additional Settings
+  # virtualisation.docker.enable = true;
+  # security.pam.services.Gabimaru.enableGnomeKeyring = false;
+  security.polkit.enable = true;
+  networking.networkmanager.enable = true;
+  hardware.bluetooth.enable = true;
+
   system.stateVersion = "26.05";
 }
+
+# gsettings set org.cinnamon.desktop.keybindings.wm switch-input-source "['<Alt>Shift_L']"
+# gsettings set org.cinnamon.desktop.keybindings.wm switch-input-source-backward "['<Shift><Alt>Shift_L']"
+
+# rm -f ~/.config/autostart/ibus.desktop
+
+# gsettings set org.freedesktop.ibus.general.hotkey triggers "@as []"
+# gsettings set org.freedesktop.ibus.general.hotkey trigger "@as []"
+# gsettings set org.freedesktop.ibus.general.hotkey next-engine "@as []"
+
+# killall ibus-daemon 2>/dev/null
+
+# gsettings get org.cinnamon.desktop.keybindings.wm switch-input-source
+# pgrep -a ibus
+
+# gsettings set org.cinnamon.desktop.keybindings.wm switch-input-source "['<Alt>Shift_L', '<Shift>Alt_L']"
